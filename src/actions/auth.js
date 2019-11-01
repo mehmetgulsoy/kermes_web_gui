fc94d554507b3caf6e301a4d0fb9106bc6d6343e
Mehmet Gülsoy <m.mehmetgulsoy@gmail.com>
2019-11-01 07:50:00 +0300

    günlük

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,39 +1,39 @@
-import { push } from 'connected-react-router'
+import { push } from "connected-react-router";
 import { getIsFetching } from "../reducers/responce";
 import * as types from "../constants/actionTypes";
 import * as MSG from "../constants/msg";
 
-
-const UserAuthSuccses = (respons) => ({
-    type: types.USER_AUTH,
-    isAuthenticated: true,
-    userName: respons.data.data.uye,
-})
+const UserAuthSuccses = respons => ({
+  type: types.USER_AUTH,
+  isAuthenticated: true,
+  userName: respons.data.data.uye
+});
 
 const UserAuthFail = () => ({
   type: types.USER_AUTH,
   isAuthenticated: false,
-  userName: '',
-})
+  userName: ""
+});
 
-export const login = (data) => (dispatch, getState, { axios, socket }) => {
+export const login = data => (dispatch, getState, { axios, socket }) => {
   if (getIsFetching(getState().responce)) {
     return Promise.resolve();
   }
   dispatch({
-    type: types.NETWORK_REQUEST_BEGIN,
+    type: types.NETWORK_REQUEST_BEGIN
   });
-  return axios.post('/login', data)
+  return axios
+    .post("/login", data)
     .then(dispatch(UserAuthFail()))
     .then(
-      respons => {       
+      respons => {
         dispatch({
           type: types.NETWORK_REQUEST_END,
           error: false,
           msg: (respons.data && respons.data.msg) || MSG.SUCCESS_MSG
         });
         dispatch(UserAuthSuccses(respons));
-        dispatch(push('/'));         
+        dispatch(push("/"));
       },
       error => {
         dispatch({
@@ -41,9 +41,9 @@ export const login = (data) => (dispatch, getState, { axios, socket }) => {
           error: true,
           msg: (error.response && error.response.data.msg) || MSG.FAIL_MSG
         });
-        dispatch(UserAuthFail());        
+        dispatch(UserAuthFail());
       }
-  );
+    );
 };
 
 export const logOut = () => (dispatch, getState, { axios, socket }) => {
@@ -51,9 +51,9 @@ export const logOut = () => (dispatch, getState, { axios, socket }) => {
     return Promise.resolve();
   }
   dispatch({
-    type: types.NETWORK_REQUEST_BEGIN,
+    type: types.NETWORK_REQUEST_BEGIN
   });
-  return axios.get('/logout').then(
+  return axios.get("/logout").then(
     respons => {
       dispatch({
         type: types.NETWORK_REQUEST_END,
@@ -71,4 +71,4 @@ export const logOut = () => (dispatch, getState, { axios, socket }) => {
       dispatch(dispatch(UserAuthFail));
     }
   );
-}; 
+};
--- src/actions/item.js
+++ src/actions/item.js
@@ -1,76 +1,78 @@
-import * as types from '../constants/actionTypes';
+import * as types from "../constants/actionTypes";
 import * as MSG from "../constants/msg";
 import { getIsFetching } from "../reducers/responce";
 import { getMenuKatagori } from "../reducers/item";
 
-export const fethUrun = () => (dispatch, getState, {axios}) => {
-  dispatch({ type: types.MENU_KATAGORI_BEGIN});
-  return axios.get('/urun_katagori').then(
-    respons => {       
-      dispatch({ type: types.MENU_KATAGORI_SUCCESS, });
+export const fethUrun = () => (dispatch, getState, { axios }) => {
+  dispatch({ type: types.MENU_KATAGORI_BEGIN });
+  return axios.get("/urun_katagori").then(
+    respons => {
+      dispatch({ type: types.MENU_KATAGORI_SUCCESS });
     },
-    error =>{
-      dispatch({ type: types.MENU_KATAGORI_FAIL}); 
-      console.log('hata: ',error);       
+    error => {
+      dispatch({ type: types.MENU_KATAGORI_FAIL });
+      console.log("hata: ", error);
     }
   );
-}
+};
 
-export const fethKatagori = () => (dispatch, getState, {axios}) => {
-  dispatch({ type: types.MENU_KATAGORI_BEGIN});
-  return axios.get('/urun_katagori').then(
-    respons => {       
-      dispatch({ type: types.MENU_KATAGORI_SUCCESS, katagori: respons.data.data.katagori.split(',')});
+export const fethKatagori = () => (dispatch, getState, { axios }) => {
+  dispatch({ type: types.MENU_KATAGORI_BEGIN });
+  return axios.get("/urun_katagori").then(
+    respons => {
+      dispatch({
+        type: types.MENU_KATAGORI_SUCCESS,
+        katagori: respons.data.data.katagori.split(",")
+      });
     },
-    error =>{
-      dispatch({ type: types.MENU_KATAGORI_FAIL}); 
-      console.log('hata: ',error);       
+    error => {
+      dispatch({ type: types.MENU_KATAGORI_FAIL });
+      console.log("hata: ", error);
     }
   );
-}
+};
 
-export const katagori_ekle = (katagori) => (dispatch, getState, {axios}) => {
+export const katagori_ekle = katagori => (dispatch, getState, { axios }) => {
   let katagoriler = getMenuKatagori(getState().item);
   katagoriler.push(katagori);
-  dispatch({ type: types.MENU_KATAGORI_BEGIN});
+  dispatch({ type: types.MENU_KATAGORI_BEGIN });
 
-  return axios.post('/katagori_ekle',katagoriler).then(
-    respons => {       
+  return axios.post("/katagori_ekle", katagoriler).then(
+    respons => {
       //dispatch({ type: types.MENU_KATAGORI_SUCCESS, katagori: katagori });
       console.log(respons);
     },
-    error =>{
-      //dispatch({ type: types.MENU_KATAGORI_FAIL}); 
-      console.log('hata: ',error);       
+    error => {
+      //dispatch({ type: types.MENU_KATAGORI_FAIL});
+      console.log("hata: ", error);
     }
   );
-}
- 
-export const saveMenuItem = (data) => (dispatch, getState, {axios}) => {
+};
+
+export const saveMenuItem = data => (dispatch, getState, { axios }) => {
   if (getIsFetching(getState().responce)) {
     return Promise.resolve();
   }
-  dispatch({type: types.NETWORK_REQUEST_BEGIN,}); 
-  return axios.post('/urun_ekle', data).then(
-    respons => {           
-      dispatch({ type: types.ITEM_SAVE_SUCCESS, item: data});
+  dispatch({ type: types.NETWORK_REQUEST_BEGIN });
+  return axios.post("/urun_ekle", data).then(
+    respons => {
+      dispatch({ type: types.ITEM_SAVE_SUCCESS, item: data });
       dispatch({
         type: types.NETWORK_REQUEST_END,
         error: false,
         msg: (respons.data && respons.data.msg) || MSG.SUCCESS_MSG
       });
     },
-    error =>{      
+    error => {
       dispatch({
         type: types.NETWORK_REQUEST_END,
         error: true,
         msg: (error.response && error.response.data.msg) || MSG.FAIL_MSG
       });
-      console.log('hata: ',error);       
+      console.log("hata: ", error);
     }
   );
-}
-
+};
 
 /*
 export const fetchTodos = (filter) => (dispatch, getState) => {
